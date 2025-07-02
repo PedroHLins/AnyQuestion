d5e441ec493171c9c24e7e65896cc7e862510f7f
PedroHLins <ped971888@gmail.com>
2025-07-02 04:42:56 -0300

    Projeto base

---
+++ anyquestion-BackEnd/src/main/java/com/github/anyquestion/AnyQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.github.anyquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyQuestionApplication.class, args);
+	}
+
+}
