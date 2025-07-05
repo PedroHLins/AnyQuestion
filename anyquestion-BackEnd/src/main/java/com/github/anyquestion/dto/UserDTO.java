bde8f835a96abd450441c18ee351d0434f1c5b04
PedroHLins <ped971888@gmail.com>
2025-07-04 22:00:02 -0300

    feat: Segurança de senha no banco de dados e mudança em DTO de class para record

--- anyquestion-BackEnd/src/main/java/com/github/anyquestion/dto/UserDTO.java
+++ anyquestion-BackEnd/src/main/java/com/github/anyquestion/dto/UserDTO.java
@@ -1,31 +1,17 @@
 package com.github.anyquestion.dto;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public class UserDTO {
-    private String email;
-    private String name;
-    private String password;
+public record UserDTO(
+        @NotBlank(message = "The field name don't be empty")
+        String name,
 
-    public String getEmail() {
-        return email;
-    }
+        @NotBlank(message = "The field email don't be empty")
+        @Email(message = "Invalid email")
+        String email,
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
+        @NotBlank(message = "The field password don't be empty")
+        @Size(min = 6, message = "The field password need to have minimum 6 characters")
+        String password
+){}
--- anyquestion-BackEnd/src/main/java/com/github/anyquestion/service/UserService.java
+++ anyquestion-BackEnd/src/main/java/com/github/anyquestion/service/UserService.java
@@ -4,21 +4,27 @@ import com.github.anyquestion.dto.UserDTO;
 import com.github.anyquestion.model.User;
 import com.github.anyquestion.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
-    public void createUser(UserDTO userDTO){
-        if(userRepository.existsByEmail(userDTO.getEmail())){
-            throw new RuntimeException();
+    public User createUser(UserDTO userDTO){
+        if(userRepository.existsByEmail(userDTO.email())){
+            throw new IllegalArgumentException("This email already exists");
         }
 
         User newUser = new User();
-        newUser.setEmail(userDTO.getEmail());
-        newUser.setName(userDTO.getName());
-        newUser.setPassword(userDTO.getPassword());
-        userRepository.save(newUser);
+        newUser.setEmail(userDTO.email());
+        newUser.setName(userDTO.name());
+        String hashPassword = passwordEncoder.encode(userDTO.password());
+        newUser.setPassword(hashPassword);
+        return userRepository.save(newUser);
     }
 }
