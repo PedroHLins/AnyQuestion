8941c5f374ba3210efbb57df065500f8c1e5698a
PedroHLins <ped971888@gmail.com>
2025-07-07 12:28:02 -0300

    feat: base para página de registro de usuários

--- anyquestion-FrontEnd/src/App.jsx
+++ anyquestion-FrontEnd/src/App.jsx
@@ -1,34 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
+
+function InputLabel(props){
+  return(
+    <>
+      <label>{props.label}</label>
+      <br />
+      <input 
+      type={props.type}
+      name={props.name}
+      placeholder={props.placeholder}
+      value={props.value}
+      onChange={props.onChange}
+      />
+      <br />
+    </>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({email: "", password: ""})
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("New user data: ", formData);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="card">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <InputLabel 
+          label="name" 
+          type="text" 
+          placeholder="example da silva" 
+          value={formData.name}
+          name="name"
+          onChange={handleChange}
+        />
+        <InputLabel 
+          label="email adress" 
+          type="email" 
+          placeholder="example@gmail.com" 
+          value={formData.email}
+          name="email"
+          onChange={handleChange}
+        />
+        <InputLabel
+          label="password"
+          type="password"
+          placeholder="example123"
+          value={formData.password}
+          name="password"
+          onChange={handleChange}
+        />
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
   )
 }
 
