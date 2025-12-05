40954d3dc55627e9edb91efd469f7daf78ffbd86
PedroHLins <ped971888@gmail.com>
2025-12-04 21:23:54 -0300

    build - commit inicial

---
+++ b_anyquestion/src/main/java/com/anyquestion/AnyquestionApplication.java
@@ -0,0 +1,11 @@
+package com.anyquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyquestionApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AnyquestionApplication.class, args);
+	}
+}
---
+++ b_anyquestion/src/test/java/com/anyquestion/AnyquestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anyquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyquestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
